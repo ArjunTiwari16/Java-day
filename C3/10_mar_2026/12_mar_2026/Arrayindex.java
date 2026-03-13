c1b840daeadca951cb35cfbdc0d2d9fe2a976009
Arjun Tiwari <at0397592@gmail.com>
2026-03-13 19:39:12 +0530

    Array Syntax

---
+++ C3/10_mar_2026/12_mar_2026/Arrayindex.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Arrayindex {
+   public static void main(String args[])
+   {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter Size of Array");
+    int size = sc.nextInt();
+    int arr[] = new int[size]; 
+
+    System.out.println("Enter "+size+" array elements");
+      for(int i=0;i<size;i++)
+       arr [i]=sc.nextInt();
+    
+    System.out.println("Array elements are:");
+      for (int i = 0; i < size; i++) 
+         System.out.print(arr[i] + " ");
+      
+      sc.close();
+   } 
+}
---
+++ C3/10_mar_2026/Demo.java
@@ -0,0 +1,7 @@
+public class Demo {
+   public static void main(String[] args){
+    int a = 677;
+    int result = a<<1;
+    System.out.println("Result of a<<1 : "+result);
+   }  
+}
